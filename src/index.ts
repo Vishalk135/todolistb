f99f600072649adfa361a77981afbaae44300950
Vishalk135 <22lco04@aitdgoa.edu.in>
2025-06-12 16:33:34 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 import taskRoutes from '../src/routes/taskRoute';
 
 const app = express();
 const PORT = 4000;
+
+const PORT = process.env.PORT || 4000;
+const JWT_SECRET = process.env.JWT_SECRET || 'default_secret';
 
 app.use(cors());
 app.use(express.json());
