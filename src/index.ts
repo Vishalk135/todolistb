ab9ccf230432d21f30d2ea43ea5f385e89d9da1b
Vishalk135 <22lco04@aitdgoa.edu.in>
2025-06-16 01:19:55 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import taskRoutes from '../src/routes/taskRoute';
 const app = express();
 const PORT = 4000;
 
-app.use(cors());
+app.use(cors({origin: ["https://todo-list-full-stack-project.netlify.app","http://localhost:4000"]}));
 app.use(express.json());
 app.use(taskRoutes);
 
